a3307e924d5114edef6e8563568349635f2dcd01
yangweixin <yangweixin@codemao.cn>
2018-07-03 19:00:54 +0800

    first commit

---
+++ src/views/user/young.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Young is Here</h1>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        }    
+    }
+</script>
+
+<style scoped lang="scss">
+    @import "@/css/user-young.scss"
+</style>
+
+
